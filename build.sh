f163da16b7b4a5138bace82837ef63c4783f4af4
cavenfang <cavenfang@tencent.com>
2022-12-08 15:03:17 +0800

    annotation

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script for server which cann't build node.js, like 2C2G.
+# You can build in your develop PC etc.
 
 # code build
 echo "-------------------- code building --------------------"
@@ -6,6 +8,6 @@ echo "-------------------- code building --------------------"
 # export NODE_OPTIONS="--max-old-space-size=1024"
 npm run build
 
-# uploading
+# upload
 echo "-------------------- uploading --------------------"
 scp -r ../garden-fe/docker/dist  root@garden:/my-workspace/garden-fe/docker/dist
